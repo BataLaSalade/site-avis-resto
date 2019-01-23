dac9c7fa1d92357561c5700af1cf832a8f8637d7
BataLaSalade <dsfmarina@gmail.com>
2019-01-24 00:46:02 +0100

    test URL constructor

--- src/app/list-restaurants/list-restaurants.component.ts
+++ src/app/list-restaurants/list-restaurants.component.ts
@@ -12,55 +12,29 @@ export class ListRestaurantsComponent implements OnInit{
   star: string = '../../assets/img/1x/emptyStar.png';
   listResto: Resto[];
   
-  constructor(private restoService: RestoService) {}
+  constructor(private restoService: RestoService) {} 
 
-  getNumberOfStars(rateScore:number) {
-    if (rateScore >=0 && rateScore <=5) {
-      return Math.round(rateScore);
-    }
-  }
-
-  hasHalfStar(ratingScore: number) {
-    let integerPartRate: number = Math.trunc(ratingScore);
-    let decimalPartRate: number = ratingScore - integerPartRate;
-    let hasHalfStar: boolean = ((decimalPartRate >= 0 && decimalPartRate <=0.25) || (decimalPartRate >= 0.75 && decimalPartRate <1)) ? false : true;
-    
-    return hasHalfStar;
-  }
-  getNameFile(ratingScore:number) {
-    let nameFile: string;
-    let integerPartRate: number = Math.trunc(ratingScore);
-    let decimalPartRate: number = ratingScore - integerPartRate;
-    let isEmpty: boolean = (decimalPartRate >= 0 && decimalPartRate < 0.25);
-    let isHalf: boolean = (decimalPartRate >= 0.25 && decimalPartRate < 0.75);
-    let isFilled: boolean = (decimalPartRate >= 0.7 && decimalPartRate <1);
-    let hasStar: boolean = true;
-    switch (hasStar) {
-      case isEmpty:
-        nameFile = "/emptyStar.png";
-        break;
-      case isHalf:
-        nameFile = "/halfStar.png";
-        break;
-      case isFilled:
-        nameFile = "/filledStar.png";
-        break;
-      default:
-        break;
-    }
-    return nameFile;
-  }
-
-  getBkgImgURL(ratingScore:number, starId: number){
+  getBkgImgURL(ratingScore:number, starId: number) {
     let starURL: string = "../../assets/img/1x/"
-    if (starId <= ratingScore) {
-      starURL += "/filledStar.png";
-      return starURL;
-    } else {
-      let nameFile: string = this.getNameFile(ratingScore);
-      starURL += nameFile;
-      return starURL;
+    let integerPartRate: number = Math.trunc(ratingScore);
+    let decimalPartRate: number = ratingScore - integerPartRate;
+    let numberOfStars: number;
+    if (decimalPartRate>=0 && decimalPartRate<0.25) { // arrondi inf
+      numberOfStars = Math.floor(ratingScore);
+      starURL += (starId<=numberOfStars) ? "/filledStar.png" : "/emptyStar.png";
+    } else if ((decimalPartRate>=0.25 && decimalPartRate<0.75)&&(starId>ratingScore)) { // je me 1/2
+      // mais si je mets un demi-etoile, je veux une étoile vide au prochain appel (normalement sur la prochaine li)
+      // or  actuellement ça me met des demi-étoiles sur les li suivante
+      // j'ai mis une variable de class pour compter le nombre de fois où je rentre dans ma condition j'ai eu un nombre totalement incohérent
+      // genre 1000++
+      //je sais que j'ai besoin d'un flag, je sais qu'il doit être mis à jour ici a chaque appel de ma fonction
+      // mais je n'arrive pas a trouver comment le faire
+      starURL += "/halfStar.png";
+    } else { // arrondi sup
+      numberOfStars = Math.ceil(ratingScore);
+      starURL += (starId<=numberOfStars) ? "/filledStar.png" : "/emptyStar.png";
     }
+    return starURL;
   }
 
   setListResto(): void {
