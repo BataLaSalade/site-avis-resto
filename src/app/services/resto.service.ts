0173a31a713f80ffcc9b3eab60e615f14a76eb58
BataLaSalade <dsfmarina@gmail.com>
2019-03-22 23:23:52 +0100

    fix details ui and refacto restoService with behaviorSubject

--- src/app/services/resto.service.ts
+++ src/app/services/resto.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Resto } from '../model/Resto';
 import { ListResto } from '../../assets/data/getResto';
-import { Observable, of, from} from 'rxjs';
+import { Observable, of, from, BehaviorSubject} from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestoService {
+  subject: BehaviorSubject<Resto[]>;
 
+  constructor() { 
+    this.subject = new BehaviorSubject(
+      new Array<Resto>()
+    )
+  }
+  
   getListResto(): Observable<Resto[]> {
-    return of (ListResto);
+    this.subject.next(ListResto)
+    return this.subject.asObservable();
   }
 
   getResto(): Observable<Resto> {
     return from (ListResto);
   }
   
-  constructor() { }
+  
 }
