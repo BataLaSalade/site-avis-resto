5e5b110ade09f0f23b396772a6a95a74ba46c4db
BataLaSalade <dsfmarina@gmail.com>
2019-04-26 21:49:48 +0200

    add some logic to get user input

--- src/app/new-resto-dialog/new-resto-dialog.component.ts
+++ src/app/new-resto-dialog/new-resto-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-new-resto-dialog',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewRestoDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<NewRestoDialogComponent>) { }
+
+  index: string[] = ["0", "1", "2", "3", "4", "5"];
+  selected: string;
+  adress: string;
+
+  onCancelClick(): void {
+    this.dialogRef.close();
+  }
+
+  onValidClick(restoNameInput): void {
+    console.log(">>> Test get Data --> restoName = ", restoNameInput, " note = ", this.selected);
+    let data = {
+      restoName: restoNameInput,
+      note: this.selected
+    }
+    this.dialogRef.close(data);
+  }
 
   ngOnInit() {
   }
