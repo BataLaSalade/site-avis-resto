5e0682606887376bdd5baefcb324b95ed11059d5
BataLaSalade <dsfmarina@gmail.com>
2019-05-03 16:50:43 +0200

    add constructor for new resto

--- src/app/model/Resto.ts
+++ src/app/model/Resto.ts
@@ -11,23 +11,15 @@ export class Resto {
     ratings?: Rate[]
     photos?: Photo[]
 
-    /* constructor(
+    constructor(
         name?: string,
-        place_id?: string,
         vicinity?: string,
         geometry?: Geometry,
-        rating?: number,
-        user_ratings_total?: number,
-        ratings?: Rate[],
-        photos?: Photo[]
+        rating?: number
     ) {
         this.name = name
-        this.place_id = place_id
         this.vicinity = vicinity
         this.geometry = geometry
         this.rating = rating
-        this.user_ratings_total = user_ratings_total
-        this.ratings = ratings
-        this.photos = photos
-    } */
+    }
 }
